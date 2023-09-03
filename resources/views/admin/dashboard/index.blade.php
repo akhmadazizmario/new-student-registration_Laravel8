c225c9b5b07543c5a0f75934bb33857501fc6fdc
akhmadazizmario <akhmadazizmario10@gmail.com>
2023-09-03 18:37:52 +0700

    news registration student

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -5,6 +5,7 @@
         <!-- Page Heading -->
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+            <a href="/pengaturan" class="ml-auto d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="bi bi-gear"></i> Pengaturan</a>
             <hr>
         </div>
 
@@ -80,8 +81,8 @@
                     </div>
                 </div>
             </div>
-             <!-- Earnings (Monthly) Card Example -->
-             <div class="col-xl-3 col-md-6 mb-4">
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
                 <div class="card border-left-dark shadow h-100 py-2">
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
@@ -98,8 +99,8 @@
                 </div>
             </div>
 
-             <!-- Earnings (Monthly) Card Example -->
-             <div class="col-xl-3 col-md-6 mb-4">
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
                 <div class="card border-left-secondary shadow h-100 py-2">
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
--- resources/views/admin/partials/navbar.blade.php
+++ resources/views/admin/partials/navbar.blade.php
@@ -153,7 +153,7 @@
                             </a>
                             <a class="dropdown-item" href="/pengaturan">
                                 <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                Settings
+                                Pengaturan
                             </a>
                             <div class="dropdown-divider"></div>
                             {{-- <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
